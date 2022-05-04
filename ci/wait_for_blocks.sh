05c567f7e6e13a2a956e1fcf10ac5e0756e26bae
mambisi <lilbizi96@gmail.com>
2022-05-04 15:00:13 +0000

    fix script

--- ci/wait_for_blocks.sh
+++ ci/wait_for_blocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 block_n=$(../src/defi-cli getblockchaininfo | jq '.blocks' )
-while (($block_n < 10)) 
+while [ "$block_n" < 10 ] 
 do
     echo $(../src/defi-cli getblockchaininfo | jq '.')
 done
