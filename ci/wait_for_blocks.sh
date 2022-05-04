d28e5ddfe2e9947671afcbf1f194adbf7ff450ee
mambisi <lilbizi96@gmail.com>
2022-05-04 12:56:18 +0000

    test

--- ci/wait_for_blocks.sh
+++ ci/wait_for_blocks.sh
@@ -2,5 +2,5 @@
 block_n=$(../src/defi-cli getblockchaininfo | jq '.blocks' )
 while (($block_n < 10)) 
 do
-    echo $(../../src/defi-cli getblockchaininfo | jq '.')
+    echo $(../src/defi-cli getblockchaininfo | jq '.')
 done
