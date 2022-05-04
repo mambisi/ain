df8ceda704de52176955854621f88a2b15f9f39b
mambisi <lilbizi96@gmail.com>
2022-05-04 20:52:49 +0000

    test sync node to 100000 blocks

--- ci/wait_for_blocks.sh
+++ ci/wait_for_blocks.sh
@@ -2,7 +2,9 @@
 block_n=$(./src/defi-cli getblockchaininfo | jq '.blocks' )
 echo $block_n
 echo $(./src/defi-cli getblockchaininfo | jq '.' )
-while [ "$block_n" -lt 10 ] 
+# Sync node to 100000 blocks
+while [ "$block_n" -lt 100000 ] 
 do
+    sleep 1
     echo $(./src/defi-cli getblockchaininfo | jq '.')
 done
