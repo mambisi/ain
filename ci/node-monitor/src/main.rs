02f1f269c0c7b5b0ac22340f155221d6883cbfe9
mambisi <lilbizi96@gmail.com>
2022-05-23 08:28:33 +0000

    sync snapshot automating

--- ci/node-monitor/src/main.rs
+++ ci/node-monitor/src/main.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+const MAX_ATTEMPS : usize = 60 * 60 * 20; // 20 mins
+
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -41,12 +43,12 @@ fn main() {
         rpcpassword = config.get("default", "rpcpassword").unwrap();
         rpcport = config.get("main", "rpcport").unwrap();
         rpcbind = config.get("main", "rpcbind").unwrap();
+    }else {
+        rpcuser = args.rpcuser.unwrap_or_default();
+        rpcpassword =args.rpcpassword.unwrap_or_default();
+        rpcport = args.rpcport.to_string();
+        rpcbind = args.rpcbind;
     }
-
-    rpcuser = args.rpcuser.unwrap_or_default();
-    rpcpassword =args.rpcpassword.unwrap_or_default();
-    rpcport = args.rpcport.to_string();
-    rpcbind = args.rpcbind;
 
 
     let rpc = Client::new(
@@ -54,25 +56,27 @@ fn main() {
         Auth::UserPass(rpcuser, rpcpassword),
     )
     .unwrap();
+    let blockchain_info = rpc.get_blockchain_info().unwrap();
 
     let mut prev_height = 0;
     let mut attempts = 0;
-    let end_height = 100000;
-    while prev_height < end_height {
+    let end_height = blockchain_info.headers;
+    println!("Node Monitor is running, TIP: {end_height}");
+    while prev_height <= end_height {
         std::thread::sleep(std::time::Duration::from_secs(1));
-        let blockchain_info = rpc.get_blockchain_info().unwrap();
-        let height = blockchain_info.blocks;
-
+        let height = rpc.get_block_count().unwrap();
         if prev_height == height {
             attempts += 1;
         } else if height > prev_height {
             attempts = 0
+        }else if height < prev_height {
+            println!("Error blocks are regessing");
+            break
         }
-        if attempts >= 60 {
-            println!("Block Stuck on {prev_height} for 60 secs");
+        if attempts >= MAX_ATTEMPS {
+            println!("Block Stuck on {prev_height} for than {MAX_ATTEMPS} secs");
             break;
         }
         prev_height = height;
-        println!("Block Height {prev_height}");
     }
 }
